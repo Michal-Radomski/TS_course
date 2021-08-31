f2cfe3e6340e1c015f2b36ea383a42ec00cfaa72
Michal Radomski <michalra@op.pl>
2021-08-31 18:20:34 +0200

    BasicTypes done

--- TS_Intro/Sekcja_2/BasicTypes.js
+++ TS_Intro/Sekcja_2/BasicTypes.js
@@ -8,3 +8,40 @@ var imaginarySalary = 1000000000;
 // Typ logiczny
 var male = true;
 var female = false;
+var x = 23;
+// Tablice
+var arr = [];
+arr.push(33);
+// Krotka
+var tuple = ["Michal", 40];
+// Any -> raczej nie używać, przydaje się przy migracji z JS do TS
+var xx;
+xx = "Michal";
+xx = 33;
+xx == {};
+xx = function () { };
+// Funkcje
+function sum(a, b) {
+    return a + b;
+}
+var s = sum(5, 5);
+var suma2 = function (a, b) { return a + b; };
+suma2(5, 5);
+// Łączenie typów -> przydaje się przy migracji
+var ux;
+ux = 5;
+ux = "X";
+ux = true;
+// Typy jako zbiory wartości
+{
+    var age_1;
+    var name_1;
+    var someVar = void 0;
+    var dice = void 0;
+    dice = 6;
+    var dontChangeMe = void 0;
+    dontChangeMe = "dom";
+    var flagIsSet = true;
+    var anotherFlag = false;
+    anotherFlag = flagIsSet;
+}
--- TS_Intro/Sekcja_2/BasicTypes.ts
+++ TS_Intro/Sekcja_2/BasicTypes.ts
@@ -10,3 +10,51 @@ let imaginarySalary: number = 1_000_000_000;
 // Typ logiczny
 let male: boolean = true;
 let female: boolean = false;
+
+let x: number = 23;
+
+// Tablice
+let arr: number[] = [];
+arr.push(33);
+
+// Krotka
+const tuple: [string, number] = ["Michal", 40];
+
+// Any -> raczej nie używać, przydaje się przy migracji z JS do TS
+let xx: any;
+xx = "Michal";
+xx = 33;
+xx == {};
+xx = () => {};
+
+// Funkcje
+function sum(a: number, b: number): number {
+  return a + b;
+}
+
+let s: number = sum(5, 5);
+
+const suma2 = (a: number, b: number): number => a + b;
+suma2(5, 5);
+
+// Łączenie typów -> przydaje się przy migracji
+let ux: number | string | boolean;
+ux = 5;
+ux = "X";
+ux = true;
+
+// Typy jako zbiory wartości
+{
+  let age: number;
+  let name: string;
+  let someVar: string | number;
+
+  let dice: 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9;
+  dice = 6;
+  let dontChangeMe: "dom";
+  dontChangeMe = "dom";
+
+  let flagIsSet: boolean = true;
+  let anotherFlag: true | false = false;
+  anotherFlag = flagIsSet;
+}
