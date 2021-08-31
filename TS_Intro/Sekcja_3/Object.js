11e09cf925e22ad5f3e2e9e4ffceef02028857d0
Michal Radomski <michalra@op.pl>
2021-08-31 20:05:31 +0200

    Object done

---
+++ TS_Intro/Sekcja_3/Object.js
@@ -0,0 +1,12 @@
+{
+    var michal = {
+        //email jest opcjonalny
+        name: "Michal",
+        age: 32
+    };
+    michal = { name: "Michal", age: 40, email: "test@op.pl" };
+    var kinga = {
+        name: "Kinga",
+        age: 34
+    };
+}
---
+++ TS_Intro/Sekcja_3/Object.ts
@@ -0,0 +1,14 @@
+{
+  let michal: {name: string; age: number; email?: string} = {
+    //email jest opcjonalny
+    name: "Michal",
+    age: 32,
+  };
+
+  michal = {name: "Michal", age: 40, email: "test@op.pl"};
+
+  let kinga: {name: string; age: number; email?: string} = {
+    name: "Kinga",
+    age: 34,
+  };
+}
